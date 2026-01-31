b5a24d588e60274c43155820f09e6203d1ae3f92
MoeinH_0 <moein24285@gmail.com>
2026-01-31 23:05:21 +0330

    Remove wwwroot generated files and apply gitignore

---
+++ DiscountManagement.Infrastructure.EFCore/Migrations/20260129194823_AddDiscount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DiscountManagement.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ColleagueDiscount",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    DiscountRate = table.Column<int>(type: "integer", nullable: false),
+                    IsRemved = table.Column<bool>(type: "boolean", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ColleagueDiscount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerDiscounts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    DiscountRate = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Reason = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDiscounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ColleagueDiscount");
+
+            migrationBuilder.DropTable(
+                name: "CustomerDiscounts");
+        }
+    }
+}
--- ServiceHost/obj/Debug/net10.0/ServiceHost.AssemblyInfo.cs
+++ ServiceHost/obj/Debug/net10.0/ServiceHost.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f07d37e00acfd9e9fdf701679b4491f98ff9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d775f5434d780766bbae7de68490f529f23cc974")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
